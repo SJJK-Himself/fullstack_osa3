97bd682b8b0f065d5afe21f52bc1554881e0bbf5
Sampo Kupiainen <sampokupiainen@gmail.com>
2022-02-19 15:20:23 +0200

    3.3 done

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let persons = [
         id: 4,
         name:"Mary Poppendick",
         number: "39-23-6423122"
-    },
+    }
 ]
 
 const contactAmt  = (persons.length)
@@ -39,6 +39,13 @@ app.get('/info', (req, res) => {
     res.send('<p>Phonebook has info for ' + contactAmt + ' people</p>' + '<p>' + currentTime + '</p>')
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {response.json(person)}
+    else {response.status(404).end()}
+})
 
 const PORT = 3001
 app.listen(PORT)
