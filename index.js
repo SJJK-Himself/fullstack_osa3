49f9af0168f3a8fa590731d7fb7481acb736f53e
Sampo Kupiainen <sampokupiainen@gmail.com>
2022-02-20 16:39:38 +0200

    3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan((tks, req, res) => {
     return [
@@ -103,6 +106,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
